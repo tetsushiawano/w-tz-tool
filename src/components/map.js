44b3f5002ea4ae7319052d9e7a8b11c30b1e0142
Tetsushi Awano <tetsushi.awano@gmail.com>
2018-07-28 23:26:10 +0900

    include d3

--- src/components/map.js
+++ src/components/map.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import * as d3 from "d3";
 
 export default class Map extends React.Component {
+    constructor(props) {
+      super(props);
+    }
 
     render () {
         return (
           <div>
             Hello, world!<br/>
-            <svg>
-            </svg>
+            <svg />
           </div>
-
         );
     }
-
 }
