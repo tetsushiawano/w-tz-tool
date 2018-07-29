a2db54b50423c774763f23ee980366534693eb92
Tetsushi Awano <tetsushi.awano@gmail.com>
2018-07-29 10:55:28 +0900

    add componentDidMount & Update

--- src/components/map.js
+++ src/components/map.js
@@ -2,15 +2,34 @@ import React from 'react'
 import * as d3 from "d3";
 
 export default class Map extends React.Component {
+
     constructor(props) {
       super(props);
+      // hoge();
+      this.hoge();
+      window.test = d3;
+      this.hoge = this.hoge.bind(this);
+    }
+
+    componentDidMount() {
+      this.hoge();
+    }
+
+    componentDidUpdate() {
+      this.hoge();
+    }
+
+    hoge() {
+        d3.select("svg")
+          .append("p")
+          .text("ddd");
     }
 
     render () {
         return (
           <div>
             Hello, world!<br/>
-            <svg />
+            <svg width="500" height="500" />
           </div>
         );
     }
